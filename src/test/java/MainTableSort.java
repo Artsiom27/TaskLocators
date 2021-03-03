e6119242889dcd1cbcef10fd3f773ce1f9ba4a62
Artsiom Tsvikevich <temancvik@gmail.com>
2021-03-04 00:50:43 +0300

    changed

--- src/test/java/MainTableSort.java
+++ src/test/java/MainTableSort.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class MainTableSort {
@@ -20,33 +22,41 @@ public class MainTableSort {
     }
 
     public static List<Employee> getEmployees(int ageMin, int salaryMax) {
+
         ArrayList<Employee> employees = new ArrayList<Employee>();
         Select dropdown = new Select(driver.findElement(By.cssSelector("label > select")));
-        dropdown.selectByVisibleText("100");
+        dropdown.selectByVisibleText("10");
 
-        List<WebElement> elements = driver.findElements(By.cssSelector("tbody > tr"));
-        for (WebElement element : elements) {
-            WebElement nameElement = element.findElement(By.cssSelector("tr > td.sorting_1"));
-            WebElement positionElement = element.findElement(By.cssSelector("tr > td:nth-child(2)"));
-            WebElement officeElement = element.findElement(By.cssSelector("tr > td:nth-child(3)"));
-            WebElement ageElement = element.findElement(By.cssSelector("tr > td:nth-child(4)"));
-            WebElement salaryElement = element.findElement(By.cssSelector("tr > td:nth-child(6)"));
+        ArrayList<WebElement> pagination = (ArrayList<WebElement>) driver.findElements(By.cssSelector("#example_paginate > span >a"));
 
-            String name = nameElement.getText();
-            String position = positionElement.getText();
-            String office = officeElement.getText();
-            String ageText = ageElement.getText();
-            int age = Integer.parseInt(ageText);
-            String salaryText = salaryElement.getText();
+        for (int i = 1; i <= pagination.size(); i++) {
 
-            String[] split = salaryText.split("[\\$ / y,]");
-            String delimiter = "";
-            String salaryString = String.join(delimiter, split);
-            int salary = Integer.parseInt(salaryString);
+            List<WebElement> elements = driver.findElements(By.cssSelector("tbody > tr"));
+            for (WebElement element : elements) {
+                WebElement nameElement = element.findElement(By.cssSelector("tr > td.sorting_1"));
+                WebElement positionElement = element.findElement(By.cssSelector("tr > td:nth-child(2)"));
+                WebElement officeElement = element.findElement(By.cssSelector("tr > td:nth-child(3)"));
+                WebElement ageElement = element.findElement(By.cssSelector("tr > td:nth-child(4)"));
+                WebElement salaryElement = element.findElement(By.cssSelector("tr > td:nth-child(6)"));
 
+                String name = nameElement.getText();
+                String position = positionElement.getText();
+                String office = officeElement.getText();
+                String ageText = ageElement.getText();
+                int age = Integer.parseInt(ageText);
+                String salaryText = salaryElement.getText();
 
-            Employee sort = new Employee(name, position, office, age, salary);
-            employees.add(sort);
+                String[] split = salaryText.split("[\\$ / y,]");
+                String delimiter = "";
+                String salaryString = String.join(delimiter, split);
+                int salary = Integer.parseInt(salaryString);
+
+                Employee sort = new Employee(name, position, office, age, salary);
+                employees.add(sort);
+
+                WebElement nextPage = driver.findElement(By.cssSelector("#example_next"));
+                nextPage.click();
+            }
         }
 
         List<Employee> employeesFilted = employees.stream()
@@ -55,6 +65,7 @@ public class MainTableSort {
                 .collect(Collectors.toList());
 
         return employeesFilted;
+
     }
 }
 
