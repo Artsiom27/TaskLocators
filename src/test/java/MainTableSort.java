d8029b840fc140f77af42b6dae4d9394f98e1f75
Artsiom Tsvikevich <temancvik@gmail.com>
2021-03-04 23:55:09 +0300

    my

--- src/test/java/MainTableSort.java
+++ src/test/java/MainTableSort.java
@@ -1,11 +1,10 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainTableSort {
@@ -27,35 +26,39 @@ public class MainTableSort {
         Select dropdown = new Select(driver.findElement(By.cssSelector("label > select")));
         dropdown.selectByVisibleText("10");
 
-        ArrayList<WebElement> pagination = (ArrayList<WebElement>) driver.findElements(By.cssSelector("#example_paginate > span >a"));
+        WebElement pagination = driver.findElement(By.cssSelector("#example_paginate"));
+        List<WebElement> allPagination = pagination.findElements(By.cssSelector("#example_paginate > span >a"));
+        WebElement nextPage = driver.findElement(By.cssSelector("#example_next"));
 
-        for (int i = 1; i <= pagination.size(); i++) {
+        if (nextPage.isEnabled()) {
+            for (int i = 0; i < allPagination.size(); i++) {
+                pagination = driver.findElement(By.cssSelector("#example_paginate"));
+                allPagination = pagination.findElements(By.cssSelector("#example_paginate > span >a"));
+                allPagination.get(i).click();
 
-            List<WebElement> elements = driver.findElements(By.cssSelector("tbody > tr"));
-            for (WebElement element : elements) {
-                WebElement nameElement = element.findElement(By.cssSelector("tr > td.sorting_1"));
-                WebElement positionElement = element.findElement(By.cssSelector("tr > td:nth-child(2)"));
-                WebElement officeElement = element.findElement(By.cssSelector("tr > td:nth-child(3)"));
-                WebElement ageElement = element.findElement(By.cssSelector("tr > td:nth-child(4)"));
-                WebElement salaryElement = element.findElement(By.cssSelector("tr > td:nth-child(6)"));
+                List<WebElement> elements = driver.findElements(By.cssSelector("tbody > tr"));
+                for (WebElement element : elements) {
+                    WebElement nameElement = element.findElement(By.cssSelector("tr > td.sorting_1"));
+                    WebElement positionElement = element.findElement(By.cssSelector("tr > td:nth-child(2)"));
+                    WebElement officeElement = element.findElement(By.cssSelector("tr > td:nth-child(3)"));
+                    WebElement ageElement = element.findElement(By.cssSelector("tr > td:nth-child(4)"));
+                    WebElement salaryElement = element.findElement(By.cssSelector("tr > td:nth-child(6)"));
 
-                String name = nameElement.getText();
-                String position = positionElement.getText();
-                String office = officeElement.getText();
-                String ageText = ageElement.getText();
-                int age = Integer.parseInt(ageText);
-                String salaryText = salaryElement.getText();
+                    String name = nameElement.getText();
+                    String position = positionElement.getText();
+                    String office = officeElement.getText();
+                    String ageText = ageElement.getText();
+                    int age = Integer.parseInt(ageText);
+                    String salaryText = salaryElement.getText();
 
-                String[] split = salaryText.split("[\\$ / y,]");
-                String delimiter = "";
-                String salaryString = String.join(delimiter, split);
-                int salary = Integer.parseInt(salaryString);
+                    String[] split = salaryText.split("[\\$ / y,]");
+                    String delimiter = "";
+                    String salaryString = String.join(delimiter, split);
+                    int salary = Integer.parseInt(salaryString);
 
-                Employee sort = new Employee(name, position, office, age, salary);
-                employees.add(sort);
-
-                WebElement nextPage = driver.findElement(By.cssSelector("#example_next"));
-                nextPage.click();
+                    Employee sort = new Employee(name, position, office, age, salary);
+                    employees.add(sort);
+                }
             }
         }
 
